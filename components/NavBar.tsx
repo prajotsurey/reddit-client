34cfca15279b0278c21b57f0a17d1c0075045573
prajotsurey <prajotsurey@gmail.com>
2022-01-24 13:10:34 +0530

    added login buttons to navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 import styled from 'styled-components'
 
 const NavBarContainer = styled.nav`
   position: fixed;
   top: 0;
-  width: 100%;
+  left:0;
+  right:0;
   height: 50px;
   background: white;
   display:flex;
@@ -23,6 +25,52 @@ const ButtonsContainer = styled.div`
   flex-direction: row;
 `
 
+const NavLink = styled.a`
+  text-decoration: none;
+  border: 1px solid #0079d3;
+  border-radius: 999px;
+  padding: 8px 40px;
+  color: #0079d3;
+  font-weight: 600;
+  margin-left: 10px;
+
+  &:hover {
+    background:#f4f9fd;
+  }
+
+  &:focus {
+    background:#eaf4fb;
+  }
+
+  &:active {
+    background: #d5e9f7;
+  }
+`
+
+const NavLinkAlternate = styled.a`
+  text-decoration: none;
+  border: 1px solid #0079d3;
+  border-radius: 999px;
+  padding: 8px 40px;
+  color: #fff;
+  font-weight: 600;
+  margin-left: 10px;
+
+  background: #0079d3;
+  
+  &:hover {
+    background:#1483d6;
+  }
+
+  &:focus {
+    background:#298eda;
+  }
+
+  &:active {
+    background: #3d99dd;
+  }
+`
+
 
 const NavBar = () => {
   return(
@@ -31,7 +79,16 @@ const NavBar = () => {
         Reddit
       </Logo>
       <ButtonsContainer>
-        
+        <Link href="/login">
+          <NavLink tabIndex={0}>
+            Log In
+          </NavLink>
+        </Link>
+        <Link href="/signup">
+          <NavLinkAlternate tabIndex={0}>
+            Sign Up
+          </NavLinkAlternate>
+        </Link>
       </ButtonsContainer>
     </NavBarContainer>
   )
