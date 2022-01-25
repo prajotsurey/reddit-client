5fe42c6055b974fa010e2e8b6b828860385aafd8
prajotsurey <prajotsurey@gmail.com>
2022-01-25 17:45:16 +0530

    configured credentials for httplink. This will allow cookies to be recieved

--- utils/client.ts
+++ utils/client.ts
@@ -1,11 +1,14 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloClient, ApolloLink, createHttpLink, InMemoryCache } from '@apollo/client'
 import { PaginatedPostsResponse } from '../generated/graphql'
 import { setContext } from '@apollo/client/link/context'
-import { getToken } from './token'
+import { getToken, setToken } from './token'
+import { TokenRefreshLink } from 'apollo-link-token-refresh'
+import jwtDecode from 'jwt-decode'
 
 console.log(process.env.NEXT_PUBLIC_SERVER_URL)
 const httpLink = createHttpLink({
   uri: process.env.NEXT_PUBLIC_SERVER_URL,
+  credentials: 'include'
 })
 
 const authLink = setContext((_, { headers }) => {
@@ -20,8 +23,43 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
+const tokenRefreshLink = new TokenRefreshLink({
+  accessTokenField: 'accessToken',
+  isTokenValidOrUndefined: () => {
+    const token = getToken()
+    if(!token){
+      return false
+    }
+
+    try {
+      const { exp }:any = jwtDecode(token)
+      if (Date.now() >= exp * 1000) {
+        return false
+      } else {
+        return true
+      }
+    } catch {
+      return false
+    }
+
+  },
+  fetchAccessToken: () => {
+    return fetch(process.env.NEXT_PUBLIC_TOKEN_REFRESH_LINK as string,{ 
+      method: 'POST',
+      credentials: 'include'
+    })
+  },
+  handleFetch: accessToken => {
+    setToken(accessToken)
+  },
+  handleError: err => {
+    console.warn('You are not logged in')
+  }
+})
+
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: ApolloLink.from([ tokenRefreshLink,authLink.concat(httpLink)]) ,
   cache: new InMemoryCache({
     typePolicies:{
       Query: {
