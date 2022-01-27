f9175c5721a3e59733e6af97d334dc36bb86f455
prajotsurey <prajotsurey@gmail.com>
2022-01-27 10:15:11 +0530

    updated dropdown svg

--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -77,7 +77,7 @@ const UserMenu = () => {
     <>
       <DropDownButton onClick={() => setOpen(!open)}>
         <DropDownSvg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect x="1.5" y="1.5" width="22" height="22" rx="1.5" strokeWidth="3"/>
+          <rect x="1.5" y="1.5" width="22" height="22" rx="1.5" strokeWidth="2"/>
           <path className='path' d="M12.5 18.3333L5.1343 10.9676C4.64615 10.4795 4.64615 9.68802 5.1343 9.19987V9.19987C5.62246 8.71171 6.41392 8.71171 6.90207 9.19987L14.2678 16.5656L12.5 18.3333Z" />
           <path className='path' d="M10.7322 16.5656L18.0979 9.19987C18.5861 8.71172 19.3775 8.71172 19.8657 9.19987V9.19987C20.3539 9.68803 20.3539 10.4795 19.8657 10.9676L12.5 18.3333L10.7322 16.5656Z" />
         </DropDownSvg>
